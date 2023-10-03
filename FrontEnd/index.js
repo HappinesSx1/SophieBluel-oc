b4dbca50072f2a79bee060313f8f4e139ad73342
unknown <david.b57970@laposte.net>
2023-10-03 03:46:56 +0200

    ending modal style

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -12,11 +12,12 @@ const lpassword = document.querySelector("#login-password");
 const submitBtn = document.querySelector(".submitbtn");
 const modifierMod = document.querySelector(".modification");
 const backMod = document.querySelector(".modal-back");
-const xMark = document.querySelector(".fa-xmark");
+const xMark = document.querySelectorAll(".fa-xmark");
 const formModal = document.querySelector(".form-modal");
 const imageModal = document.getElementById("file");
 const titreModal = document.getElementById("title-modal");
 const categorieModal = document.getElementById("categorie-modal");
+const displayModal = document.querySelector(".display-works-modal");
 
 async function fetcher() {
   await fetch(`http://localhost:5678/api/works`)
@@ -24,6 +25,7 @@ async function fetcher() {
     .then((data) => (works = data));
 
   filterWorksByCategory(currentCategory);
+  galeriesDisplayModal(works);
 }
 
 function galeriesDisplay(filteredWorks) {
@@ -31,7 +33,7 @@ function galeriesDisplay(filteredWorks) {
     .map(
       (work) =>
         `
-        <figure>
+        <figure id=${work.id}>
           <img src=${work.imageUrl}>
           <figcaption>${work.title}</figcaption>
         </figure>
@@ -76,43 +78,35 @@ backMod.addEventListener("click", () => {
   document.getElementById("modal").style.display = "none";
 });
 
-xMark.addEventListener("click", () => {
-  document.getElementById("modal").style.display = "none";
-});
+xMark.forEach((mark) =>
+  mark.addEventListener("click", () => {
+    document.getElementById("modal").style.display = "none";
+  })
+);
 
 /* modal */
 
+/* navigation in the modal */
+
+const modalAdding = document.querySelector(".modal-adding");
+const modalDelete = document.querySelector(".modal-delete");
+const addImages = document.querySelector(".add-images");
+const arrowLeft = document.querySelector(".back-modal");
+
+addImages.addEventListener("click", () => {
+  modalAdding.style.display = "block";
+  modalDelete.style.display = "none";
+});
+
+arrowLeft.addEventListener("click", () => {
+  modalAdding.style.display = "none";
+  modalDelete.style.display = "block";
+});
+
+/* adding images */
+
 formModal.addEventListener("submit", (event) => {
   event.preventDefault();
-
-  // const data = {
-  //   title: titreModal.value,
-  //   image: imageModal.files[0].name,
-  //   category: categorieModal.value,
-  // };
-
-  // console.log(data);
-
-  // fetch("http://localhost:5678/api/works", {
-  //   method: "POST",
-  //   headers: {
-  //     Accept: "application/json",
-  //     Authorization: `Bearer ${localStorage.getItem("token de connexion")}`,
-  //     "Content-Type": "multipart/form-data",
-  //   },
-  //   body: JSON.stringify(data),
-  // })
-  //   .then((res) => {
-  //     console.log(res);
-  //     return res.json();
-  //   })
-  //   .then((data) => {
-  //     console.log(data);
-  //   })
-  //   .catch((error) => {
-  //     console.error("Erreur lors de la requête:", error);
-  //   });
-
   const file = imageModal.files[0];
 
   // Vérifiez si un fichier a été sélectionné
@@ -148,3 +142,54 @@ formModal.addEventListener("submit", (event) => {
     console.error("Aucun fichier sélectionné.");
   }
 });
+
+/* DELETE SECTION */
+
+function galeriesDisplayModal(worksModal) {
+  displayModal.innerHTML = worksModal
+    .map(
+      (work) =>
+        `
+        <figure id=${work.id} class="works">
+          <i class="fa-solid fa-trash-can"></i>
+          <img src=${work.imageUrl}>
+        </figure>
+        `
+    )
+    .join("");
+
+  const figures = document.querySelectorAll(".works");
+
+  figures.forEach(function (figure) {
+    figure.addEventListener("click", function (event) {
+      // Obtenez l'ID de l'élément sur lequel le clic a eu lieu
+      const elementId = event.currentTarget.id;
+
+      // Faites quelque chose avec l'ID (par exemple, affichez-le dans la console)
+      console.log("ID de l'élément cliqué :", elementId);
+      const requestOptionsDelete = {
+        method: "DELETE",
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token de connexion")}`,
+        },
+      };
+
+      fetch(
+        `http://localhost:5678/api/works/${elementId}`,
+        requestOptionsDelete
+      );
+    });
+  });
+}
+
+/* gestion login-side of the site */
+
+const modifieBtn = document.querySelector(".modification");
+const tri = document.querySelector(".tri");
+
+const connected = localStorage.getItem("token de connexion") ? true : false;
+
+if (connected) {
+  modifieBtn.style.display = "flex";
+  tri.style.display = "none";
+}
